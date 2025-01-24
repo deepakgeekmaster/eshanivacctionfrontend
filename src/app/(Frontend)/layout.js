69bacfba7fa7c60268743c1f6b642e40aef0b738
deepakgeekmaster <deepakgeekmaster@gmail.com>
2025-01-24 11:39:15 +0530

    Update layout.js

--- src/app/(Frontend)/layout.js
+++ src/app/(Frontend)/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={asap.className}>
      <head>
-    <!-- Google tag (gtag.js) --> <script async src="https://www.googletagmanager.com/gtag/js?id=G-RPKTWGXKVX"></script> <script>   window.dataLayer = window.dataLayer || [];   function gtag(){dataLayer.push(arguments);}   gtag('js', new Date());   gtag('config', 'G-RPKTWGXKVX'); </script>
+     <script async src="https://www.googletagmanager.com/gtag/js?id=G-RPKTWGXKVX"></script> <script>   window.dataLayer = window.dataLayer || [];   function gtag(){dataLayer.push(arguments);}   gtag('js', new Date());   gtag('config', 'G-RPKTWGXKVX'); </script>
  
     </head>
 
