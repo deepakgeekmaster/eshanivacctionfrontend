0b6ba5eeb62ba9bf4cd8870df2d7fe2702a7a175
deepakgeekmaster <deepakgeekmaster@gmail.com>
2025-01-24 11:42:14 +0530

    Update layout.js

--- src/app/(Frontend)/layout.js
+++ src/app/(Frontend)/layout.js
@@ -20,10 +20,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={asap.className}>
-     <head>
-     <script async src="https://www.googletagmanager.com/gtag/js?id=G-RPKTWGXKVX"></script> <script>   window.dataLayer = window.dataLayer || [];   function gtag(){dataLayer.push(arguments);}   gtag('js', new Date());   gtag('config', 'G-RPKTWGXKVX'); </script>
- 
-    </head>
+      <head>
+        {/* Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-RPKTWGXKVX%22>"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RPKTWGXKVX');
+          `}
+        </Script>
+      </head>
 
         <main className="py-4">
           <Header />
